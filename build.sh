8d68c54b63e9bf946c86f3f432035d13a75c1b5f
root <root@ip-172-31-17-31.us-west-2.compute.internal>
2022-01-24 07:18:45 +0000

    update build.sh files for new camelcase directory names

--- build.sh
+++ build.sh
@@ -1,22 +1,28 @@
+echo ""
 echo "**********************************************************************"
 echo "$(TZ=America/Los_Angeles date) - Git pull commencing..."
 echo "**********************************************************************"
 git pull
 
+echo ""
 echo "**********************************************************************"
 echo "$(TZ=America/Los_Angeles date) - Build front end commencing..."
 echo "**********************************************************************"
 
-cd jcinfo-frontend
+cd frontend
+npm i
 npm run build
 
+echo ""
 echo "**********************************************************************"
 echo "$(TZ=America/Los_Angeles date) - Build back end commencing..."
 echo "**********************************************************************"
 
-cd ../JCInfo-Backend
+cd ../backend
 ./build.sh
 
+echo ""
 echo "**********************************************************************"
 echo "$(TZ=America/Los_Angeles date) - Build finished."
 echo "**********************************************************************"
+echo ""
