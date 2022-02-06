88d48179ffd1cf00cf4139bc8bed0508e709cd22
Abel Wenning <abelw@live.com>
2022-02-05 22:06:49 -0800

    update both build.sh scripts

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,23 @@
+cd opt/jcinfo/backend
+
+echo "*** dotnet publish -c Release"
 dotnet publish -c Release
-docker stop $(docker ps -a -q --filter ancestor=jcinfo-backend)
+
+runningDockerProcess=$(docker ps -a -q --filter ancestor=jcinfo-backend)
+# if Non-zero-length string, then...
+if [ -n "$runningDockerProcess" ] then
+    docker stop $runningDockerProcess
+else
+    echo ""
+    echo "*** No Docker process is running with the jcinfo-backend image."
+    echo ""
+fi
+
+echo "*** docker rmi jcinfo-backend"
 docker rmi jcinfo-backend
+
+echo "*** docker build --tag jcinfo-backend ."
 docker build --tag jcinfo-backend .
+
+echo "*** docker run --rm -dt -p 127.0.0.1:5000:80 jcinfo-backend jcinfo-backend"
 docker run --rm -dt -p 127.0.0.1:5000:80 jcinfo-backend jcinfo-backend
--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
+# execute via: /opt/jcinfo/build.sh | tee -a '/opt/jcinfo/build.log'
+# -a means append
+
+
 echo ""
 echo "**********************************************************************"
 echo "$(TZ=America/Los_Angeles date) - Git pull commencing..."
 echo "**********************************************************************"
+cd /opt/jcinfo
 git pull
 
 echo ""
@@ -10,7 +15,9 @@ echo "$(TZ=America/Los_Angeles date) - Build front end commencing..."
 echo "**********************************************************************"
 
 cd frontend
+echo "*** npm i"
 npm i
+echo "*** npm run build"
 npm run build
 
 echo ""
@@ -19,6 +26,7 @@ echo "$(TZ=America/Los_Angeles date) - Build back end commencing..."
 echo "**********************************************************************"
 
 cd ../backend
+echo "*** ./build/sh"
 ./build.sh
 
 echo ""
