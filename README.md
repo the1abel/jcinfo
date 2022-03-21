a85535c67d7a35b2e6d18e5f6319a2a693c1d15a
Abel Wenning <abelw@live.com>
2022-03-20 18:34:02 -0700

    update backend/build.sh and README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jcinfo
+
+See `devops` repo's README.md for details.
--- backend/build.sh
+++ backend/build.sh
@@ -22,14 +22,17 @@ echo "*** docker build --tag jcinfo-backend ."
 docker build --tag jcinfo-backend .
 
 # start image as a process
-echo "*** docker run --rm -dt -p localhost:5000:80 jcinfo-backend jcinfo-backend"
+echo "*** docker run ... jcinfo-backend"
 # Parameters:
 #   --rm=delete when stopped
 #   -d=detached
 #   -t=tail logs
 #   -p=port
+#   --env-file=file containing environment variables to be available in container
 #   --name=process name
 #   (positional)<image-name>
-docker run --rm -dt -p localhost:5000:80 --name jcinfo-backend jcinfo-backend
+docker run --rm -dt -p 127.0.0.1:5000:80 \
+--env-file /root/env.list \
+--name jcinfo-backend jcinfo-backend
 # To view processes: docker ps -a
 # To follow/tail logs: docker logs -f <process-name>
